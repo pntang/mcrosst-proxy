1775030ea285f901587ed7bf2a4570efbf9e50a6
Sprinkle Tang <65575548+pntang@users.noreply.github.com>
2023-01-11 08:42:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'mcrosst.urcraft.repl.co'
+      url.hostname = 'play.urcraft.repl.co'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
